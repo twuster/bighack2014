777cef89a8b2e2f0515dd7e3693d05e724d1aa53
Tony Wu <tony.wu@berkeley.edu>
2014-04-06 04:05:13 -0700

    added polyline stuff

--- src/com/example/BigHack2014/CongratulationActivity.java
+++ src/com/example/BigHack2014/CongratulationActivity.java
@@ -3,8 +3,10 @@ package com.example.BigHack2014;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +14,17 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -32,6 +40,7 @@ public class CongratulationActivity extends Activity implements GooglePlayServic
     GoogleMap map;
     RunsDataSource datasource;
     Context context;
+    ArrayList<Parcelable> points;
     /**
      * Called when the activity is first created.
      */
@@ -39,8 +48,22 @@ public class CongratulationActivity extends Activity implements GooglePlayServic
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.congratulation_layout);
+        Bundle extras = getIntent().getExtras();
+        if (extras!=null){
+            points = extras.getParcelableArrayList("points");
+        }
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         map.setMyLocationEnabled(true);
+
+        PolylineOptions options = new PolylineOptions();
+        for (int i =0; i<points.size(); i++){
+            options.add((LatLng)points.get(i));
+        }
+        Polyline line = map.addPolyline(options
+                .width(5)
+                .color(Color.BLUE));
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(new LatLngBounds.Builder().build(), 16));
+
         mLocationClient = new LocationClient(this, this, this);
         nameText = (EditText)findViewById(R.id.nameText);
         datasource = new RunsDataSource(this);
