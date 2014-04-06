9c75d5e341d8497936907a6684cd18ab0c6eaeab
Vincent Tian <vincenttian16@gmail.com>
2014-04-05 23:32:35 -0700

    ignoring tinder for now

--- src/com/example/BigHack2014/DetailView.java
+++ src/com/example/BigHack2014/DetailView.java
@@ -8,9 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.kindredprints.android.sdk.KindredOrderFlow;
-import com.kindredprints.android.sdk.KLOCPhoto; // use this
-import com.kindredprints.android.sdk.KindredOrderFlowActivity;
+//import com.kindredprints.android.sdk.KindredOrderFlow;
+//import com.kindredprints.android.sdk.KLOCPhoto; // use this
+//import com.kindredprints.android.sdk.KindredOrderFlowActivity;
 
 import java.io.FileOutputStream;
 
@@ -25,35 +25,35 @@ public class DetailView extends Activity{
         setContentView(R.layout.detail_view);
 
         Button printButton = (Button)findViewById(R.id.printButton);
-        printButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                KindredOrderFlow orderFlow  = new KindredOrderFlow(context, "eXv0gT0jqHLo0sB8Ku9v7UFo");
+//        printButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                KindredOrderFlow orderFlow  = new KindredOrderFlow(context, "eXv0gT0jqHLo0sB8Ku9v7UFo");
 
 //                Save image to disc and give path
-                FileOutputStream out = null;
-                Intent intent = getIntent();
-                try {
-                    out = new FileOutputStream((String) intent.getExtras().get("message"));
+//                FileOutputStream out = null;
+//                Intent intent = getIntent();
+//                try {
+//                    out = new FileOutputStream((String) intent.getExtras().get("message"));
 //                    intent.getExtras().get("map").compress(Bitmap.CompressFormat.PNG, 90, out);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try{
-                        out.close();
-                    } catch(Throwable ignore) {}
-                }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                } finally {
+//                    try{
+//                        out.close();
+//                    } catch(Throwable ignore) {}
+//                }
 
-                orderFlow.addImageToCart(new KLOCPhoto(null, (String) intent.getExtras().get("message")));
-                Intent i = new Intent(getApplicationContext(), KindredOrderFlowActivity.class);
-                startActivityForResult(i, 0);
-            }
-        });
+//                orderFlow.addImageToCart(new KLOCPhoto(null, (String) intent.getExtras().get("message")));
+//                Intent i = new Intent(getApplicationContext(), KindredOrderFlowActivity.class);
+//                startActivityForResult(i, 0);
+//            }
+//        });
 
 
 
         Intent intent = getIntent();
-        super.onCreate(savedInstanceState);
+//        super.onCreate(savedInstanceState);
         String name = (String) intent.getExtras().get("message");
 //        String date = (String) intent.getExtras().get("date");
 //        String map = (String) intent.getExtras().get("map");
