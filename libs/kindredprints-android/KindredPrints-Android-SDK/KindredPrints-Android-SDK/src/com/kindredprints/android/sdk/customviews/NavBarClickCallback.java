7b54318e29d7b46019a128592b7c848f63650b4c
Vincent Tian <vincenttian16@gmail.com>
2014-04-05 22:06:56 -0700

    implement kindred printing with filepaths

--- libs/kindredprints-android/KindredPrints-Android-SDK/KindredPrints-Android-SDK/src/com/kindredprints/android/sdk/customviews/NavBarClickCallback.java
+++
@@ -1,6 +0,0 @@
-package com.kindredprints.android.sdk.customviews;
-
-public interface NavBarClickCallback {
-	public void onBackClick();
-	public void onNextClick();
-}
--- libs/kindredprints-android/KindredPrints-Android-SDK/KindredPrints-Android-SDK/src/com/kindredprints/android/sdk/remote/NetworkCallback.java
+++
@@ -1,7 +0,0 @@
-package com.kindredprints.android.sdk.remote;
-
-import org.json.JSONObject;
-
-public interface NetworkCallback {
-	public void finished(JSONObject serverResponse);
-}
