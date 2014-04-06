03223bccf1f75ed3118359044108b22394e29692
Tony Wu <tony.wu@berkeley.edu>
2014-04-06 11:40:43 -0700

    TINDER

--- src/com/example/BigHack2014/DetailView.java
+++ src/com/example/BigHack2014/DetailView.java
@@ -7,20 +7,23 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.kindredprints.android.sdk.KLOCPhoto;
+import com.kindredprints.android.sdk.KindredOrderFlow;
+import com.kindredprints.android.sdk.KindredOrderFlowActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-//import com.kindredprints.android.sdk.KindredOrderFlow;
-//import com.kindredprints.android.sdk.KLOCPhoto; // use this
-//import com.kindredprints.android.sdk.KindredOrderFlowActivity;
-
 public class DetailView extends Activity{
 
     public Context context;
+    private final static String KINDRED_APP_KEY = "test_eXv0gT0jqHLo0sB8Ku9v7UFo";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,36 +31,11 @@ public class DetailView extends Activity{
         context = this;
         setContentView(R.layout.detail_view);
 
-        Button printButton = (Button)findViewById(R.id.printButton);
-//        printButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                KindredOrderFlow orderFlow  = new KindredOrderFlow(context, "eXv0gT0jqHLo0sB8Ku9v7UFo");
-
-//                Save image to disc and give path
-//                FileOutputStream out = null;
-//                Intent intent = getIntent();
-//                try {
-//                    out = new FileOutputStream((String) intent.getExtras().get("message"));
-//                    intent.getExtras().get("map").compress(Bitmap.CompressFormat.PNG, 90, out);
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                } finally {
-//                    try{
-//                        out.close();
-//                    } catch(Throwable ignore) {}
-//                }
-
-//                orderFlow.addImageToCart(new KLOCPhoto(null, (String) intent.getExtras().get("message")));
-//                Intent i = new Intent(getApplicationContext(), KindredOrderFlowActivity.class);
-//                startActivityForResult(i, 0);
-//            }
-//        });
 
 
 
         Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
+        final Bundle extras = intent.getExtras();
         String name = (String) extras.get("message");
         String bitmap = (String) extras.get("bitmap");
         Date date = (Date) extras.get("date");
@@ -70,6 +48,19 @@ public class DetailView extends Activity{
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String parsedDate = dateFormat.format(date);
         dateText.setText(parsedDate);
+
+
+        Button printButton = (Button)findViewById(R.id.printButton);
+        printButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KindredOrderFlow orderFlow  = new KindredOrderFlow(context, KINDRED_APP_KEY);
+                String path = Environment.getExternalStorageDirectory().toString() + "/" +  extras.get("bitmap");
+                orderFlow.addImageToCart(new KLOCPhoto(null, path));
+                Intent i = new Intent(getApplicationContext(), KindredOrderFlowActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
     }
 
     public Bitmap loadBitmap(String file){
@@ -78,6 +69,15 @@ public class DetailView extends Activity{
         String path = Environment.getExternalStorageDirectory().toString() + "/" + file;
         Bitmap bitmap = BitmapFactory.decodeFile(path, options);
         return bitmap;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == KindredOrderFlowActivity.KP_RESULT_CANCELLED) {
+            Log.i("MyActivity", "User cancelled Kindred purchase");
+        } else if (resultCode == KindredOrderFlowActivity.KP_RESULT_PURCHASED) {
+            Log.i("MyActivity", "User completed Kindred purchase!");
+        }
     }
 
 }
