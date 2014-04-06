5a57c87adc381dc7d2071673cc841de19b59df8f
Tony Wu <tony.wu@berkeley.edu>
2014-04-05 19:31:41 -0700

    changed onclick functionality

--- src/com/example/BigHack2014/MyActivity.java
+++ src/com/example/BigHack2014/MyActivity.java
@@ -1,12 +1,16 @@
 package com.example.BigHack2014;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
-public class MyActivity extends Activity implements View.OnClickListener {
+public class MyActivity extends Activity{
+
+    Context context;
     /**
      * Called when the activity is first created.
      */
@@ -14,32 +18,41 @@ public class MyActivity extends Activity implements View.OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        context = this;
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.johnbutton:
-
-                break;
-            case R.id.vincentbutton:
-                Intent myIntent = new Intent(this,MapListView.class);
-                this.startActivity(myIntent);
-                break;
-            case R.id.nishantbutton:
-
-                break;
-            case R.id.tonybutton:
+        Button tonyButton = (Button)findViewById(R.id.tonybutton);
+        tonyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Log.d("tonybutton", "PRESSING BUTTON");
-                Intent i = new Intent(this, CongratulationActivity.class);
-                this.startActivity(i);
-                break;
-            default:
-                break;
-
-
-        }
+                Intent i = new Intent(context, CongratulationActivity.class);
+                context.startActivity(i);
+            }
+        });
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch(v.getId()){
+//            case R.id.johnbutton:
+//
+//                break;
+//            case R.id.vincentbutton:
+//                Intent myIntent = new Intent(this,MapListView.class);
+//                this.startActivity(myIntent);
+//                break;
+//            case R.id.nishantbutton:
+//
+//                break;
+//            case R.id.tonybutton:
+//
+//                break;
+//            default:
+//                break;
+//
+//
+//        }
+//    }
 
     public void openListView(View view) {
         Intent myIntent = new Intent(this,MapListView.class);
